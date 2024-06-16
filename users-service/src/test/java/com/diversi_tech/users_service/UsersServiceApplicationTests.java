43531074475ddabaec732bd819754a296e61ce84
shiraCarmi <shirac325@gmail.com>
2024-06-16 13:33:11 +0300

    begining

---
+++ users-service/src/test/java/com/diversi_tech/users_service/UsersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diversi_tech.users_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
